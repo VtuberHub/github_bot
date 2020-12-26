9408578670291cbe4035215c8864132befc4826e
b1indsight <zerolingling0@gmail.com>
2020-12-26 20:37:01 +0800

    update yml

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+    try {
+    // `who-to-greet` input defined in action metadata file
+        const myToken = core.getInput('github-token')
+        const octokit = github.getOctokit(myToken)
+        const labelDict = core.getInput('keyword-labels')
+        const time = (new Date()).toTimeString();
+
+        const payload = JSON.stringify(github.context.payload, undefined, 2);
+        var ourIssueOrPR = github.context.payload.issue;
+        if (ourIssueOrPR == null) {
+            ourIssueOrPR = github.context.payload.pull_request;}
+
+        let fetchedTitle = ourIssueOrPR.title.slice().toLowerCase();
+        let fetchedBody = ourIssueOrPR.body.slice().toLowerCase();
+
+        var labels = [];
+        for (key in labelDict) {
+
+        }
+
+        octokit.issues.addLabels({
+            issue: ourIssueOrPR, 
+            labels: labels
+        })
+
+        core.setOutput("time", time);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+run()
